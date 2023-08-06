b800df0d3347a70db0e6b99ea830681874f067d5
Akash S <akashaki0860@gmail.com>
2023-08-06 19:10:55 +0530

    3rd commit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,11 +9,15 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    numbers = request.json
+    response = numbers['first'] + numbers['second']
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    numbers = request.json
+    response = numbers['first'] + numbers['second']
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
